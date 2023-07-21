51f115438746b0472d359c781e964c648db2ba5a
Savan <savansavani111@gmail.com>
2023-07-21 16:31:10 -0400

    mern stripe integration

---
+++ src/components/PayButton.jsx
@@ -0,0 +1,28 @@
+import axios from "axios";
+import { useSelector } from "react-redux";
+import { url } from "../slices/api";
+
+const PayButton = ({ cartItems }) => {
+  const user = useSelector((state) => state.auth);
+
+  const handleCheckout = () => {
+    axios
+      .post(`${url}/stripe/create-checkout-session`, {
+        cartItems,
+        userId: user._id,
+      })
+      .then((res) => {
+        if (res.data.url) {
+          window.location.href = res.data.url;
+        }
+      })
+      .catch((err) => console.log(err));
+  };
+  return (
+    <div>
+      <button onClick={() => handleCheckout()}>Check Out</button>
+    </div>
+  );
+};
+
+export default PayButton;
---
+++ src/slices/api.js
@@ -0,0 +1,11 @@
+export const url = "https://stripe-backend-eight.vercel.app/api";
+
+export const setHeaders = () => {
+  const headers = {
+    headers: {
+      "x-auth-token": localStorage.getItem("token"),
+    },
+  };
+
+  return headers;
+};
